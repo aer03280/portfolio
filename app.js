dc437e4e02bd35aa3846434091da16c63c1d8883
Ashley Ross <ashley.e.ross@gmail.com>
2016-11-30 10:54:53 -0800

    built constructor for project list

---
+++ app.js
@@ -0,0 +1,47 @@
+var projectData = [
+  {
+    title: 'About Me',
+    preview: 'https://placekitten.com/300/300',
+    previewUrl: 'https://github.com/aer03280/about_me',
+    dateCreated: '2016-18-10',
+    description: '<p>This is a site I built with a get-to-know Ashley game.</p>',
+  },
+  {
+    title: 'Trip Planner',
+    preview: 'https://placekitten.com/300/300',
+    previewUrl: 'https://github.com/shortaj/Group_Project-Trip-Planner',
+    dateCreated: '2016-09-11',
+    description: '<p>This application was created as a group assignment. It helps you select a destination for your next vacation.</p>',
+
+  },
+  {
+    title: 'The Cookie Stand',
+    preview: 'https://placekitten.com/300/300',
+    previewUrl: 'https://github.com/aer03280/cookie-stand',
+    dateCreated: '2016-24-10',
+    description: '<p>I created this site as a class assignment for a fictitious cookie shop.</p>',
+  }
+
+];
+
+var articles = [];
+
+function Articles (projects) {
+  this.title = projects.title;
+  this.preview = projects.preview;
+  this.previewUrl = projects.previewUrl;
+  this.dateCreated = projects.dateCreated;
+  this.description = projects.description;
+}
+
+
+Article.prototype.toHtml = function() {
+  var $newArticle = $('article.projectStyle').clone();
+  $newArticle.find('h1').text(this.title);
+  $newArticle.find('time.pubdate').attr('title', this.dateCreated);
+  $newArticle.find('div a').attr('href', this.previewUrl);
+  $newArticle.find('div img').attr('src', this.preview);
+  $newArticle.find('section.summary').html(this.description);
+  $newArticle.removeClass('projectStyle');
+  return $newArticle;
+}
