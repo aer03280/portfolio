cd060da78e7644458152899a63e562418ac87563
Ashley Ross <ashley.e.ross@gmail.com>
2016-12-02 10:12:58 -0800

    moved files to new folders and updated README

--- scripts/app.js
+++ scripts/app.js
@@ -39,15 +39,7 @@ Articles.prototype.toHtml = function() {
   var $source = $('#project-template').html();
   var templateRender = Handlebars.compile($source);
   return templateRender(this);
-  // var $newArticle = $('article.projectStyle').clone();
-  // $newArticle.find('h1:first').text(this.title);
-  // $newArticle.find('.previewBox img').attr('src', this.preview);
-  // $newArticle.find('.previewBox a').attr('href', this.previewUrl);
-  // $newArticle.find('section.summary').addClass('summaryTest').html(this.description);
-  // $newArticle.find('time.pubdate:first').attr('title', this.dateCreated);
   $newArticle.find('time').text('created ' + parseInt((new Date() - new Date(this.dateCreated))/60/60/24/1000) + ' days ago.');
-  // $newArticle.removeClass('projectStyle');
-  // return $newArticle;
 }
 
 
@@ -62,8 +54,6 @@ projectData.forEach(function(artObj){
 arts.forEach(function(a) {
   $('#projects').append(a.toHtml());
 });
-
-// var projectView = {};
 
 
 projectData.handleNav = function () {
