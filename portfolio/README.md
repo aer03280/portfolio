6bcb24affdcbd364c991278a87f38e644cef1e64
Ashley Ross <ashley.e.ross@gmail.com>
2016-12-15 09:01:39 -0800

    adding portfolio not sure if this is right

---
+++ portfolio/README.md
@@ -0,0 +1,25 @@
+# portfolio
+**Author**: Ashley Ross
+
+## Description
+Portfolio of Ashley's work
+
+### List of files
+
+- index.html
+- base.css
+- modules.css
+- layout.css
+- app.js
+- projectView.js
+- README.md
+- LICENSE
+
+## Summary
+
+This portfolio was created as a Codefellows 301 assignment to showcase my work.
+
+Some of the objectives are:
+- list projects in order was most recent displaying first
+- Practice jQuery and Handlebars
+- RWD - Use media queries to adapt site for different screen sizes
---
+++ portfolio/scripts/app.js
@@ -0,0 +1,53 @@
+var proj = [];
+function Projects (opts) {
+  for (key in opts) {
+    this[key] = opts[key];
+  }
+};
+
+Projects.prototype.toHtml = function() {
+  this.daysAgo = parseInt((new Date() - new Date(this.dateCreated))/60/60/24/1000) + ' days ago.';
+  this.publishStatus = this.dateCreated ? 'created ' + this.daysAgo: '(draft)';
+  var $source = $('#project-template').html();
+  var templateRender = Handlebars.compile($source);
+  return templateRender(this);
+}
+
+projectData.sort(function(currentObject, nextObject) {
+  return(new Date(nextObject.dateCreated)) - (new Date(currentObject.dateCreated));
+});
+
+projectData.forEach(function(artObj){
+  proj.push(new Projects(artObj));
+});
+
+proj.forEach(function(a) {
+  $('#projects').append(a.toHtml());
+});
+
+
+// use JSON to store data in localStorage
+Projects.fetchAll = function() {
+  if(localStorage.projectData) {
+    Projects.loadAll(JSON.parse(localStorage.projectData));
+    articleView.renderPage();
+  } else {
+    $.getJSON('data/projectData.json', function(data) {
+      localStorage.projectData = JSON.stringify(data);
+      Projects.loadAll(JSON.parse(localStorage.projectData));
+      articleView.renderPage();
+    });
+  }
+};
+
+// Hide about and home tabs when the other is clicked // currently greyed bc routes.js
+// projectData.handleNav = function () {
+//   $('.navMain').on('click' , '.nav' , function() {
+//     $('.navContent').hide();
+//     $('#' + $(this).attr('data-content')).fadeIn(700);
+//   });
+//   $('.navMain .nav:first').click();
+// };
+//
+// projectData.handleNav();
+Projects.fetchAll();
