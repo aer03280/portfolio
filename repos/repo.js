a961886032e35a9dfd1fbe5dea7c5bb6b167f096
Ashley Ross <ashley.e.ross@gmail.com>
2016-12-15 17:16:02 -0800

    called functions in app.js but may need to update href in html

---
+++ repos/repo.js
@@ -0,0 +1,20 @@
+(function(module) {
+  var reposObj = {};
+
+  reposObj.requestRepos = function(callback) {
+    // NOTE: refactor this request into an $.get call
+    $.when(
+     $.get('/github/users/aer03280/repos', function(data) {
+       reposObj.allRepos = data;
+     })
+    ).done(callback);
+  };
+
+  reposObj.withTheAttribute = function(attr) {
+    return reposObj.allRepos.filter(function(aRepo) {
+      return aRepo[attr];
+    });
+  };
+
+  module.reposObj = reposObj;
+})(window);
